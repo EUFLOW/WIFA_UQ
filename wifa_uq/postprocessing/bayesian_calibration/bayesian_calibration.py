afabe562f9dfbe04b0042552686ef6b5748a5d58
Frederik Aerts <u0161035@kuleuven.be>
2025-09-24 15:49:56 +0200

    put all data in examples/data

--- wifa_uq/postprocessing/bayesian_calibration/bayesian_calibration.py
+++ wifa_uq/postprocessing/bayesian_calibration/bayesian_calibration.py
@@ -9,11 +9,36 @@ except:
 
 class BayesianCalibration(PostProcesser):
 
-    def __init__(self):
-        pass
+    def __init__(self, system_yaml, params, data):
+        self.system_yaml = system_yaml
+        self.params = params
+        self.data = data
+
+        self.flow_model = umbra.flow_model.WIFAModel(open(system_yaml))
+        print(self.flow_model.name_model)
+
+        self.__initialize_bayesian_model()
+
+
+    def __initialize_bayesian_model(self):
+        # Initalize prior
+        params = []
+        dists = []
+        for param, range in self.params.items():
+            assert (len(range) == 2) and (range[0]<range[1])
+            params.append(param)
+            dists.append(umbra.distributions.Uniform(range[0], range[1]))
+        prior = umbra.bayesian_model.Prior(params, dists)
+        # Initalize likelihood
+        likelihood = umbra.bayesian_model.LikelihoodTurbinePower(flow_model=self.flow_model, data=self.data)
+        # Initialize bayesian model
+        self.bayesian_model = umbra.bayesian_model.BayesianModel(prior, likelihood, name='umbra4wifa')
 
     def fit(self):
-        pass
+        sampler = umbra.sampler.TMCMC(self.bayesian_model, parallel=False)
+        inference_data = sampler.sample()
+        return inference_data.samples
+
 
     def predict(self):
         pass
